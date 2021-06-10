483b4cb7b52b60b5f53515e8d6108c154a459c24
cm <chenmeng_1919@163.com>
2021-06-10 13:37:02 +0800

    合并增加力扣官方解题方法

--- src/main/java/EightyEight.java
+++ src/main/java/EightyEight.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
+
 /*
  * @author: cm
  * @date: Created in 2021/6/10 10:31
  * @description:合并两个有序数组
  */
+
 public class EightyEight {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
-        EightyEight.merge(nums1,3,nums2,3);
+        EightyEight.merge(nums1, 3, nums2, 3);
     }
+
+
+    /**
+     * @description:自己完成的方法
+     * @Param: [nums1, m, nums2, n]
+     * @Return: void
+     * @Date: 2021/6/10 13:35
+     */
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = 0;
         int p2 = 0;
@@ -37,5 +48,70 @@ public class EightyEight {
         for (int i = 0; i < m + n; i++) {
             nums1[i] = a[i];
         }
+    }
+
+    /**
+     * @description:方法一：直接合并后排序。
+     * @Param: [nums1, m, nums2, n]
+     * @Return: void
+     * @Date: 2021/6/10 13:29
+     */
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i != n; ++i) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    /**
+     * @description:方法二：双指针
+     * @Param: [nums1, m, nums2, n]
+     * @Return: void
+     * @Date: 2021/6/10 13:33
+     */
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
+    }
+
+    /**
+     * @description:方法三：逆向双指针
+     * @Param: [nums1, m, nums2, n]
+     * @Return: void
+     * @Date: 2021/6/10 13:35
+     */
+    void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
+            }
+            nums1[tail--] = cur;
+        }
+
     }
 }
