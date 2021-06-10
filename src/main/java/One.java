e45641263fb36d8e12718f42158e81feebb898d6
cm <chenmeng_1919@163.com>
2021-06-10 14:20:42 +0800

    两数之和添加官方解法

--- src/main/java/One.java
+++ src/main/java/One.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @author: cm
  * @date: Created in 2021/4/12 16:04
@@ -14,6 +17,13 @@ public class One {
         }
     }
 
+
+    /**
+     * @description:自定义
+     * @Param: [nums, target]
+     * @Return: int[]
+     * @Date: 2021/6/10 14:10
+     */
     public int[] twoSum(int[] nums, int target) {
         int[] a;
         int i = 0;
@@ -26,5 +36,41 @@ public class One {
             }
         }
         return nums;
+    }
+
+    /**
+     * @description:方法一：暴力枚举
+     * @Param: [nums, target]
+     * @Return: int[]
+     * @Date: 2021/6/10 14:10
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+    /**
+     * @description:方法二：哈希表
+     * @Param: [nums, target]
+     * @Return: int[]
+     * @Date: 2021/6/10 14:11
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
