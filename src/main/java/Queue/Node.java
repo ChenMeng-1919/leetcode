30470751895e1b5d43a581d4b6b929c1964d5770
cm <chenmeng_1919@163.com>
2021-11-15 15:51:41 +0800

    622. 设计循环队列

---
+++ src/main/java/Queue/Node.java
@@ -0,0 +1,16 @@
+package Queue;
+
+/*
+ * @author: cm
+ * @date: Created in 2021/11/15 15:40
+ * @description:单向链表
+ */
+public class Node {
+    public int value;
+    public Node nextNode;
+
+    public Node(int value) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
